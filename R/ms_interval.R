760f42668dfbae21866328c4f27141c6375754da
Tony Vashevko <avashevko@gmail.com>
2021-07-21 10:47:26 +0800

    use mpfr a little more efficiently

--- R/ms_interval.R
+++ R/ms_interval.R
@@ -36,6 +36,47 @@ msik.baseline <- function(kd, ki = kd, d = 1) {
     c.atan + (c.d + (c.logp + c.logn))
 }
 
+#' Market Share - Interval; k (exponentiated) formulation; naive mpfr-wrapped
+#'
+#' @param kd value at focal pole of decreasing items
+#' @param ki value at focal pole of increasing items
+#' @param d length of interval
+msik.baseline.mpfr.wrap <- function(kd, ki = kd, d = 1, prec = 1024) {
+    msik.baseline(mpfr(kd, prec), mpfr(ki, prec), mpfr(d, prec))
+}
+
+#' Market Share - Interval; k (exponentiated) formulation; fully inside mpfr land
+#'
+#' @param kd value at focal pole of decreasing items
+#' @param ki value at focal pole of increasing items
+#' @param d length of interval
+msik.full.mpfr <- function(kd, ki = kd, d = 1, prec = 1024) {
+    # vectorize arguments
+    vargs <- vectorize.args(kd = kd, ki = ki, d = d)
+    kd.m <- mpfr(vargs$kd, prec)
+    ki.m <- mpfr(vargs$ki, prec)
+    d.m <- mpfr(vargs$d, prec)
+    # keep certain things outside mpfr
+    # discriminant: positive = arctan; negative = arctanh
+    disc <- 4 * vargs$kd * vargs$ki - 1
+    #disc <- 4 * kd.m * ki.m - 1
+    # universal arctan(h) argument constructor
+    base <- sqrt(abs(disc))
+    #base <- mpfr(abs(disc), prec)
+    arg <- arg.atanx(kd.m, ki.m, d.m)
+    suppressMessages({
+        c.atan <- dplyr::if_else(abs(disc) < .Machine$double.eps,
+            -arg,
+            dplyr::if_else(disc > 0,
+                -atan(base * arg) / base,
+                -atanh(base * arg) / base))
+    })
+    # add them in an order that hopefully minimizes catastrophic cancellation
+    c.atan + 0.5*(d.m + (log1p(kd.m + ki.m) - log1p(kd.m*exp(-d.m) + ki.m*exp(d.m))) )
+}
+
+#msik.rcpp <- function(kd, ki = kd, d = 1, prec = 1024)
+
 #' Market Share - Interval; k (exponentiated) formulation
 #'
 #' @param kd value at focal pole of decreasing items
@@ -44,7 +85,8 @@ msik.baseline <- function(kd, ki = kd, d = 1) {
 #' @importFrom Rmpfr mpfr asNumeric
 #' @export
 msik <- function(kd, ki, d, prec = 1024) {
-    asNumeric(msik.baseline(mpfr(kd, prec), mpfr(ki, prec), mpfr(d, prec)))
+    #asNumeric(msik.baseline(mpfr(kd, prec), mpfr(ki, prec), mpfr(d, prec)))
+    asNumeric(msik.full.mpfr(kd, ki, d, prec))
 }
 
 #' Market Share - Interval; v (base-quality) formulation
@@ -58,8 +100,13 @@ msik <- function(kd, ki, d, prec = 1024) {
 #' @export
 msiv <- function(v0s, vds, vis, d) {
     ss <- relative.share(v0s, vds)
-    mss <- msik(kd = exp(v0s) + sum(exp(vds)),
-                ki = sum(exp(vis)),
+    kd <- exp(v0s) + sum(exp(vds))
+    ki <- sum(exp(vis))
+    #mss <- msik(kd = exp(v0s) + sum(exp(vds)),
+    #            ki = sum(exp(vis)),
+    #            d = d)
+    mss <- msik(kd = kd,
+                ki = ki,
                 d = d)
     ss * mss
 }
